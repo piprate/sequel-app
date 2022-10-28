5e6728bcaa98c47ad7f917bc0cba472dca2336b7
Stan Nazarenko <stanislav@binaryorder.com>
2022-10-28 12:02:15 +0100

    Set correct URL for tags + rationalise timeline URL construction

--- src/routes/_api/timelines.js
+++ src/routes/_api/timelines.js
@@ -3,58 +3,44 @@ import { base, sequelAuth } from './utils'
 import { populateNotificationMediaURLs, populatePostMediaURLs } from './media'
 import { unwrap } from '../_utils/mapper'
 
-function getTimelineUrlPath (timeline) {
-  switch (timeline) {
-    case 'notifications':
-    case 'notifications/mentions':
-      return 'notification/timeline'
-    case 'bookmarks':
-      return 'spark'
-    case 'subscriptions':
-    case 'subscriptions/exclusive':
-      return 'spark'
-  }
-  if (timeline.startsWith('tag/')) {
-    return 'timelines/tag'
-  } else if (timeline.startsWith('spark/')) {
-    return 'spark'
-  } else if (timeline.startsWith('world/')) {
-    return 'world'
-  } else if (timeline.startsWith('bubble/')) {
-    return 'bubble'
-  }
-  throw new Error(`Invalid timeline type: ${timeline}`)
-}
-
 export async function getTimeline (instanceName, accessToken, asSpark, timeline, maxId, since, limit) {
-  const timelineUrlName = getTimelineUrlPath(timeline)
-  let url = `${base(instanceName, accessToken)}/${timelineUrlName}`
+  let url = `${base(instanceName, accessToken)}/`
 
-  const isNotification = timelineUrlName === 'notification/timeline'
+  const isNotification = timeline.startsWith('notifications')
 
   const params = {}
 
   if (timeline.startsWith('tag/')) {
-    url += '/' + timeline.split('/')[1]
-  } else if (timeline.startsWith('spark/')) {
-    url += '/' + timeline.split('/')[1] + '/timeline'
-  } else if (timeline.startsWith('world/')) {
-    url += '/' + timeline.split('/')[1] + '/timeline'
-  } else if (timeline.startsWith('bubble/')) {
-    url += '/' + timeline.split('/')[1] + '/timeline'
+    let parts = timeline.split('/')
+    url += `${parts[0]}/${encodeURIComponent(parts[1])}/timeline`
+  } else if (timeline.startsWith('spark/') || timeline.startsWith('world/') || timeline.startsWith('bubble/')) {
+    let parts = timeline.split('/')
+    url += `${parts[0]}/${parts[1]}/timeline`
+    if (timeline.endsWith('media')) {
+      params.only_media = true
+    } else {
+      params.include_comments = timeline.endsWith('/with_comments')
+    }
   } else if (timeline === 'bookmarks') {
     if (!asSpark) {
       return { items: [], headers: [] }
     }
-    url += '/' + unwrap(asSpark) + '/post-bookmarks'
+    url += 'spark/' + unwrap(asSpark) + '/post-bookmarks'
   } else if (timeline.startsWith('subscriptions')) {
     if (!asSpark) {
       return { items: [], headers: [] }
     }
-    url += '/' + unwrap(asSpark) + '/subscription-timeline'
+    url += 'spark/' + unwrap(asSpark) + '/subscription-timeline'
     if (timeline === 'subscriptions/exclusive') {
       params.exclusive_only = true
     }
+  } else if (isNotification) {
+    url += 'notification/timeline'
+    if (timeline === 'notifications/mentions') {
+      params.include_types = ['mention', 'comment']
+    }
+  } else {
+    throw new Error(`Invalid timeline type: ${timeline}`)
   }
 
   if (since) {
@@ -69,21 +55,9 @@ export async function getTimeline (instanceName, accessToken, asSpark, timeline,
     params.limit = limit
   }
 
-  if (timeline.startsWith('spark/') || timeline.startsWith('world/') || timeline.startsWith('bubble/')) {
-    if (timeline.endsWith('media')) {
-      params.only_media = true
-    } else {
-      params.include_comments = timeline.endsWith('/with_comments')
-    }
-  }
-
-  if (timeline === 'notifications/mentions') {
-    params.include_types = ['mention', 'comment']
-  }
-
   url += '?' + paramsString(params)
 
-  console.log('fetching url', url)
+  console.log('Fetching url', url)
   const {
     json: items,
     headers
