77be2c4773ebcba82da7eeb2c9c9b4042d6f817d
andreynikolic <118774509+andreynikolic@users.noreply.github.com>
2023-04-26 00:31:14 +0200

    Bug fix: post not refreshed on update

--- src/routes/_components/list/ListLazyItem.svelte
+++ src/routes/_components/list/ListLazyItem.svelte
@@ -2,6 +2,7 @@
   import ListItem from './ListItem.svelte'
   import { mark, stop } from '../../_utils/marks'
   import { createEventDispatcher, onMount } from "svelte";
+  import { on } from '../../_utils/eventBus';
 
   export let component;
   export let index;
@@ -13,12 +14,17 @@
 
   let props = undefined;
 
-  onMount(async () => {
+  async function fetchProps () {
     if (makeProps) {
       props = await makeProps(key);
       mark('ListLazyItem set props');
       stop('ListLazyItem set props');
     }
+  }
+
+  onMount(async () => {
+    fetchProps()
+    return on('postUpdated', fetchProps);
   });
 </script>
 
